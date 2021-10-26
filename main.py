c728b4d8a0394e5d6eac2f68ed6474eacb8eabcb
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-26 17:08:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ for question in question_data:
 # quiz = QuizBrain(question_bank)
 # quiz_ui = QuizInterface(quiz)
 
-#     quiz.next_question()
 
 # print("You've completed the quiz")
 # print(f"Your final score was: {quiz.score}/{quiz.question_number}")
