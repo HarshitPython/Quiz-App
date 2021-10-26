32298d10a1823b2e94c37981b603618f0affe203
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-26 17:08:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
-#     question_bank.append(new_question)
+    question_bank.append(new_question)
 
 
 # quiz = QuizBrain(question_bank)
