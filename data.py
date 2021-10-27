7fbabbaac5c044a8742d3e6dac665faaac2ab8c9
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-27 17:26:06 +0530

    Update data.py

--- data.py
+++ data.py
@@ -57,17 +57,7 @@ question_data = data["results"]
 #     {
 #         "category": "Science: Computers",
 #         "type": "boolean",
-#         "difficulty": "easy",
-#         "question": "Linus Torvalds created Linux and Git.",
-#         "correct_answer": "True",
-#         "incorrect_answers": [
-#             "False"
-#         ]
-#     },
-#     {
-#         "category": "Science: Computers",
-#         "type": "boolean",
-
+# 
 #      
 #         
 #         
