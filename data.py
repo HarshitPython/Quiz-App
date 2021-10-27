5860e3077a5d270d1ec22070940f6d20293e85f8
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-27 17:24:48 +0530

    Update data.py

--- data.py
+++ data.py
@@ -92,10 +92,5 @@ question_data = data["results"]
 #         "correct_answer": "True",
 #         "incorrect_answers": [
 #             "False"
-#         ]
-#     },
-#     {
-#         "category": "Science: Computers",
-#         "type": "boolean",
-#         "difficulty": "easy",
+#         
 #         
