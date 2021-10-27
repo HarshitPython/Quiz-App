b5808e2d6f6e97ed9f2e70ee224e1878cd2429e5
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-27 17:24:58 +0530

    Update data.py

--- data.py
+++ data.py
@@ -87,10 +87,6 @@ question_data = data["results"]
 #     {
 #         "category": "Science: Computers",
 #         "type": "boolean",
-#         "difficulty": "easy",
-#         "question": "'HTML' stands for Hypertext Markup Language.",
-#         "correct_answer": "True",
-#         "incorrect_answers": [
-#             "False"
+#         
 #         
 #         
