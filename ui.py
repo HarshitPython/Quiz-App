c82c1bb29c6d42752d3e94176345c1a1732bd1e7
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-28 12:53:38 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -17,12 +17,12 @@ class QuizInterface():
 
         self.canvas = Canvas(width=300,height=250,bg="white")
         self.question_text = self.canvas.create_text(
-#             150,
-#             125,
-#             width=280,
-#             text="some question text",
-#             fill=THEME_COLOR,
-#             font=["Arial",20,"italic"]
+            150,
+            125,
+            width=280,
+            text="some question text",
+            fill=THEME_COLOR,
+            font=["Arial",20,"italic"]
 #             )
 #         self.canvas.grid(row=1, column=0,columnspan=2,pady=50)
 
